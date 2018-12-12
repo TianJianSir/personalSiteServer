0fa21ad065622ac32a53889d1c49e7f825ac1706
jason.tian <jason.tian@ximalaya.com>
2018-12-12 15:45:44 +0800

    mongodb

--- api/user.js
+++ api/user.js
@@ -1,13 +1,30 @@
 let express = require('express')
 let router = express.Router()
+const MongoClient = require('mongodb').MongoClient;
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'blogs';
 
 router.get('/',function(req,res){
-    let data = {
-        username: 'this is user',
-        sex: 'man',
-        address: '上海'
-    }
-    res.end(JSON.stringify(data))
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+       
+        const db = client.db(dbName);
+        console.log(db)
+       
+        client.close();
+
+        let data = {
+            username: 'this is user',
+            sex: 'man',
+            address: '上海'
+        }
+        res.end(JSON.stringify(data))
+      });
 })
 
 router.post('/',function(req,res){
